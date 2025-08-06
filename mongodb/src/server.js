d02dbbf1f0e8a5e103b9db8229f6dd6b59272f08
Ale Coss <alexacoss99@gmail.com>
2025-08-06 14:52:41 -0600

    Creacion routes

--- mongodb/src/server.js
+++ mongodb/src/server.js
@@ -1,17 +1,22 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import router from './routes/users.js'
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+// Middleware
+app.use(express.json())
+app.use('/api', router)
+
 app.get('/', (req, res) => {
   res.send("Hola Atlas")
 })
 
-//connect() pide una uri o llave que conecte con la base de datos
+// connect() pide una uri o llave que conecte con la base de datos
 mongoose
 .connect(process.env.MONGODB_KEY)
 .then( () => console.log("Conectado a Mongo DB Atlas") )
